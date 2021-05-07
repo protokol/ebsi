388167502f7cb530c4a122b4f1d32f6ebcc951d3
Amadej Pevec <amadej.pevec@gmail.com>
2021-05-07 12:48:36 +0200

    chore: remove unused code

--- packages/notarization-transactions/__tests__/functional/transaction-forging/__support__/transaction-factory.ts
+++ packages/notarization-transactions/__tests__/functional/transaction-forging/__support__/transaction-factory.ts
@@ -11,12 +11,6 @@ export class NotarizationTransactionFactory extends TransactionFactory {
         return new NotarizationTransactionFactory(app);
     }
 
-    public withVendorField(vendorField: string): NotarizationTransactionFactory {
-        this.builder.vendorField(vendorField);
-
-        return this;
-    }
-
     public Notarization(notarization: Interfaces.INotarizationAsset): NotarizationTransactionFactory {
         this.builder = new Builders.NotarizationBuilder().Notarization(notarization);
 
